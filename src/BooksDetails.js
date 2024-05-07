f2dc37f729dfbbbe8012479b16a9794afa1813ea
treyjeyoghera <treyjeyogheraeugene@gmail.com>
2024-05-07 16:59:05 +0300

    space

--- src/BooksDetails.js
+++ src/BooksDetails.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-//import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 const BookDetails = () => {
   const { bookId } = useParams();
   const [book, setBook] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  
 
   
   useEffect(() => {
