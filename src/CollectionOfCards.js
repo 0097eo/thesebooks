7210147dd018b85a05e2a777d2d0bf63d28e28ac
Sedly <sedly.keith@student.moringaschool.com>
2024-05-07 13:07:21 +0300

    CollectionOfCards.js

--- src/CollectionOfCards.js
+++ src/CollectionOfCards.js
@@ -3,4 +3,22 @@ import Card from './Card';
 
 const CollectionOfCards = () => {
   const [books, setBooks] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);}
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://booksdata.onrender.com/books')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setBooks(data);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.error(error);
+        setIsLoading(false);
+      });
+  }, []);}
