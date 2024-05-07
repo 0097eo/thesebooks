c2033be73d0f526e211f4ca7ab6cb8c4a20ad3af
Patrick Njuguna <mpatricknjuguna02@gmail.com>
2024-05-07 23:25:23 +0300

    Implimentation of Navigation links

---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {link} from 'react-router-dom';
+const Navbar = () => {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">Books</Link>
+          </li>
+          <li>
+            <Link to="/newbook">Add New Book</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
